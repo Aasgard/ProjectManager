13f8d9a719a2040e1a40ce6b4c6e0afa5d65bf2c
flancien <francois.regis.lancien@gmail.com>
2018-01-17 14:05:12 +0100

    Ajout des assets et commencement du Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { MailChooserComponent } from '../components/mail-chooser/mail-chooser.component';
+import { HomePageComponent } from '../pages/home-page/home-page.component';
+import {RouterModule, Routes} from '@angular/router';
+import { ProjectListPageComponent } from '../pages/project-list-page/project-list-page.component';
+
+const appRoutes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent},
+  { path: 'project-list', component: ProjectListPageComponent}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    MailChooserComponent
+    MailChooserComponent,
+    HomePageComponent,
+    ProjectListPageComponent
   ],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
